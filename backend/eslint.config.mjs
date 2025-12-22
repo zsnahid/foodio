82bce5ec875c64c87973bcb2cfab01fa1ed1f0a8
zsnahid <zahidsadmannahid@gmail.com>
2025-12-22 22:14:15 +0600

    Synchronise local git with github

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -29,7 +29,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'prettier/prettier': ['off', { endOfLine: 'auto' }],
     },
