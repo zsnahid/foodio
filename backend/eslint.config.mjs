3f937f070de7748a89bb213f29667c92dfdddcf5
zsnahid <zahidsadmannahid@gmail.com>
2025-12-22 21:47:10 +0600

    Modified some ESLint rules from 'error' to 'warn'

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -29,7 +29,9 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'prettier/prettier': ['off', { endOfLine: 'auto' }],
     },
   },
 );
